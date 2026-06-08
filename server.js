5c1004653dc0168c8b0a309788b46753307e6be6
bhargavvwxyz <bhargav.brahmadesam@trysqueez.com>
2026-06-08 23:30:15 +0530

    socketsend - sends to client when connected

--- server.js
+++ server.js
@@ -2,4 +2,14 @@ const Websocket = require("ws");
 
 const wss = new Websocket.Server({port:8080})
 
-console.log("Websocket connected")
+
+wss.on("connection",(socket)=>{
+
+
+    console.log("client connected")
+
+    socket.send("connected");
+
+   
+
+})
